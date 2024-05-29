102cd1e730bf1f79f15209b7ad8aa43f73c9be69
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-05-29 16:13:40 +0530

    learn basics

---
+++ Basics/addition.js
@@ -0,0 +1,9 @@
+//normal addition method in js
+
+var a=10
+var b=46
+var c =a+b
+//we can execute js files in two ways
+
+console.log(c)
+//document.write(c)
---
+++ Basics/data-1.js
@@ -0,0 +1,14 @@
+//to perform CRUD operations
+
+//create variable
+var eid =101;
+
+//read variable
+console.log(eid)
+
+//update variable
+
+eid="Rajini Kanth"
+
+//delete variable
+delete eid
