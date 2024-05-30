e69b61f2945d912811abd4a89cd71165442f1941
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-05-30 18:26:13 +0530

    test

---
+++ Basics/KeyWords/Learn_keyword.js
@@ -0,0 +1,13 @@
+var eid =101
+function cal_age(){}
+class employee{}
+let emp={}
+
+//invalid names
+//let typeof ="Rahul Ganghi"; // typeof is keyword
+//let this = 102
+//let th!s=104;
+//let super ="Super Star"
+
+let constructor="Hello"
+console.log(constructor)
