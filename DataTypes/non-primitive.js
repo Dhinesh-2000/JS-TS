5dd8263d8c5eb70b69cdbbdb84f9301b10d9b8c4
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-06-03 19:04:42 +0530

    test

---
+++ DataTypes/non-primitive.js
@@ -0,0 +1,17 @@
+//create
+var size=[38.40,42,44]
+var emp ={eid:101,ename:"Rahul"}
+var spec={fit:"Regular",size:40}
+
+//read
+console.log(size);
+console.log(emp);
+console.log(spec);
+
+//to find data type
+console.log(typeof size);
+console.log(typeof emp);
+console.log(typeof spec);
+
+
+
