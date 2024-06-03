7f3828124b3dfd68fe1101038e7d03b1c6c0ac30
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-06-03 18:29:11 +0530

    test

---
+++ DataTypes/cr_types.js
@@ -0,0 +1,14 @@
+// to create 
+var eid =101
+var ename='Rahul'
+
+//to read the module
+
+console.log(eid);
+console.log(ename);
+
+
+///to find the data type
+
+console.log(typeof eid);
+console.log(typeof ename);
