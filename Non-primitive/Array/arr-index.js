4b8a6fd136bbcc9c88ac9a3759f552b6ba5d40bb
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-06-04 11:19:03 +0530

    learn Array

--- Non-primitive/Array/arr-index.js
+++ Non-primitive/Array/arr-index.js
@@ -9,6 +9,6 @@ console.log(enames[1]);
 console.log(enames[2]);
 console.log(enames[3]);
 console.log(enames[4]);
-console.log(enames[5]);
-console.log(enames[6]);
-console.log(enames[7]);
+console.log(enames[5]);  //if the value is not there in
+console.log(enames[6]);    //index value it will print 
+console.log(enames[7]); // the value undefined.
