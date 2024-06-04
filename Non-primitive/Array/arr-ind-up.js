72b94c94ee842deca9987252f33fc172b4a1a197
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-06-04 11:30:47 +0530

    learn Array

---
+++ Non-primitive/Array/arr-ind-up.js
@@ -0,0 +1,17 @@
+let enames =['Rahul','Sonia','Priyanka','Modi','Pradosh']
+
+
+console.log(enames[0]);
+console.log(enames[1]);
+console.log(enames[2]);
+console.log(enames[3]);
+console.log(enames[4]);
+console.log(enames[75]);
+
+
+//update
+
+enames[0]="Rahul Gandhi"
+enames[4]="pradosh Mammutty"
+
+console.log(enames);
