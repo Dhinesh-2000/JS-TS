90bda3413d61dce256d04d66847e898f41dab993
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-06-04 11:13:47 +0530

    learn Array

---
+++ Non-primitive/Array/arr-2.js
@@ -0,0 +1,5 @@
+let sizes=[38,40,42,44]
+console.log(sizes);
+
+let enames =['Rahul','Sonia','Priyanka','Modi','Stalin']
+console.log(enames);
---
+++ Non-primitive/Array/arr-index.js
@@ -0,0 +1,14 @@
+//reading arr elements - using indexing
+let enames =['Rahul','Sonia','Priyanka','Modi','Stalin']
+console.log(enames);
+
+//to read array elements - using indexing
+
+console.log(enames[0]);
+console.log(enames[1]);
+console.log(enames[2]);
+console.log(enames[3]);
+console.log(enames[4]);
+console.log(enames[5]);
+console.log(enames[6]);
+console.log(enames[7]);
