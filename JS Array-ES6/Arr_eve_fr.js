6bb155abbf47c914c92433d43cfadbdbecabb425
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-06-25 10:40:36 +0530

    texr

---
+++ JS Array-ES6/Arr_eve_fr.js
@@ -0,0 +1,4 @@
+let numbers =[1,2,3,4,5,6,7,8,9,10]
+
+let even_num=numbers.filter((num)=>{ return num%2==0})
+console.log(even_num);
---
+++ JS Array-ES6/Prices_map.js
@@ -0,0 +1,7 @@
+let prices=[98,198,298,398]
+
+let new_prices=prices.map((price)=>{
+    return price+1
+})
+console.log(new_prices);
+
