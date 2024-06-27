9bc758d21e1834a576bd398ed95044be5715ebd1
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-06-27 10:28:29 +0530

    test

---
+++ DeStructuring/ObjDest/one.js
@@ -0,0 +1,9 @@
+let emp ={
+    id:101,
+    name:"Rahul Gandhi",
+    sal:45000,
+    loc:"Wayanad",
+    email:"rahul@gmail.com"
+}
+
+console.log(emp.id);
---
+++ DeStructuring/ObjDest/two.js
@@ -0,0 +1,11 @@
+let emp ={
+    id:101,
+    name:"Rahul Gandhi",
+    sal:45000,
+    loc:"Wayanad",
+    email:"rahul@gmail.com"
+}
+
+let {id,sal}=emp
+console.log(id);
+console.log(sal);
