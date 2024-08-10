477b096e9506b7e79d56b2d879b8fd5c27e1ee83
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-08-10 14:43:08 +0530

    test

---
+++ Part-1/JS-iter/ex-2.js
@@ -0,0 +1,5 @@
+let num =' ';
+for(let i=1;i<=5;i++){
+    num+=i +" ";
+}
+console.log(num);
---
+++ Part-1/JS-iter/ex-3.js
@@ -0,0 +1,16 @@
+let eids=[101,102,103]
+
+for ( eid of eids) {
+    console.log(eid);
+}
+
+
+let emp={
+    "id":101,
+    "name":"Rahul",
+    "sal":45000
+}
+
+for(prop in emp){
+    console.log(emp[prop]);
+}
