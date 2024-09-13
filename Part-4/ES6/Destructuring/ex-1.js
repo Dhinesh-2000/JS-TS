9c8c6f5b34d85bfc16e113e543430736bb51ed3c
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-09-13 10:49:03 +0530

    tes't

---
+++ Part-4/ES6/Destructuring/ex-1.js
@@ -0,0 +1,13 @@
+let emp={
+    "id":101,
+    "ename":'Rahul',
+    "email":"rahul@gmail.com"
+}
+
+//destructuring
+
+let {id,ename,email}= emp;
+
+console.log(id);
+console.log(ename);
+console.log(email);
---
+++ Part-4/ES6/Destructuring/ex-2.js
@@ -0,0 +1,20 @@
+let state={
+    emp:{
+    "id":101,
+    "ename":'Rahul',
+    "email":"rahul@gmail.com"
+    },
+    product:{
+        p_Name:"Iphone 16 pro"
+    },
+    count:100,
+    users:[]
+}
+
+let {count}=state;
+
+console.log(count);
+// console.log(product.p_Name);
+
+
+// console.log(state.emp.ename);
