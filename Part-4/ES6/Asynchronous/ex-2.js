ae5246c37813e70116b629d93a8424bc000e5d05
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-09-14 21:11:11 +0530

    test

---
+++ Part-4/ES6/Asynchronous/ex-2.js
@@ -0,0 +1,5 @@
+setTimeout(()=>{console.log("Request 1")},4000)
+setTimeout(()=>{console.log("Request 2")},3000)
+setTimeout(()=>{console.log("Request 3")},5000)
+setTimeout(()=>{console.log("Request 4")},1000)
+setTimeout(()=>{console.log("Request 5")},2000)
