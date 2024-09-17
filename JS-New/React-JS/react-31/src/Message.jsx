f5e213a9711979e3404298e5b531107082f712ce
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-09-17 12:45:18 +0530

    yh

---
+++ JS-New/React-JS/react-31/src/Message.jsx
@@ -0,0 +1,8 @@
+let Message=()=>{
+    return <div>
+        <h4>Message Component</h4>
+    </div>
+
+}
+
+export default Message
---
+++ JS-New/React-JS/react-31/src/index.js
@@ -0,0 +1,6 @@
+import ReactDom from 'react-dom'
+import App from './App'
+
+let root=ReactDom.createRoom(document.getElementById('root'))
+
+root.render(<App/>)
