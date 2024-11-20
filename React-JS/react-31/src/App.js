5b69eb8863e55fbd54a8e91e81676ece805b28c2
Dhinesh-2000 <dhineshmailsamy2000@gmail.com>
2024-11-20 19:25:34 +0530

    test

--- React-JS/react-31/src/App.js
+++ React-JS/react-31/src/App.js
@@ -4,11 +4,15 @@ import Welcome from "./Components/Welcome"
 import Hello from "./Components/Hello"
 import Message from "./Components/Message"
 import Counter from "./Components/Counter"
+import FunctionClick from "./Components/FunctionClick"
+import ClassClick from "./Components/ClassClick"
+import EventBind from "./Components/EventBind"
+import ParentComponent from "./Components/ParentComponent"
 
 class App extends Component{
   render(){
   return( <div className="App">
-    <h1>App Component</h1>
+    {/* <h1>App Component</h1>
      <hr/>
     <Greet name ="Dhinesh" surname="Batman" />
     <Greet name ="Pranav" surname="SpiderMan"/>
@@ -17,11 +21,16 @@ class App extends Component{
      <Welcome name="Dhinesh" surname="Batman"/>
      <Welcome name ="Pranav" surname="SpiderMan"/>
      <Welcome name="Sara" surname="Super Women"/>
-    <hr/> 
+    <hr/>  */}
    {/* <Hello/> */}
      
     {/* <Message/> */}
     {/* <Counter/> */}
+    {/* <FunctionClick/>
+    <hr/>
+    <ClassClick/> */}
+    {/* <EventBind/> */}
+    <ParentComponent/>
     
   </div>)
   }
